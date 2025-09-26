97737723800653aa5fc811b554d16118179d94b2
Mara Delgadillo <84547390+MaraD98@users.noreply.github.com>
2025-09-26 16:22:36 -0300

    Creacion del controller y Command

---
+++ HybridDDDArchitecture/Application/UseCases/Automovil/Commands/CreateAutomovil/CreateAutomovilCommand.cs
@@ -0,0 +1,17 @@
+﻿using Core.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Automovil.Commands.CreateAutomovil
+{
+    public class CreateAutomovilCommand : IRequestCommand<string>
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+    }
+
+}
---
+++ HybridDDDArchitecture/Template-API/Controllers/AutomovilController.cs
@@ -0,0 +1,27 @@
+﻿using Application.UseCases.Automovil.Commands.CreateAutomovil;
+using Core.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers
+{
+    [ApiController]
+    public class AutomovilController : BaseController
+    {
+        private readonly ICommandQueryBus _commandQueryBus;
+
+        public AutomovilController(ICommandQueryBus commandQueryBus)
+        {
+            _commandQueryBus = commandQueryBus ?? throw new ArgumentNullException(nameof(commandQueryBus));
+        }
+
+        [HttpPost("api/v1/[controller]")]
+        public async Task<IActionResult> Create(CreateAutomovilCommand command)
+        {
+            if (command is null) return BadRequest();
+
+            var id = await _commandQueryBus.Send(command);
+
+            return Created($"api/v1/[controller]/{id}", new { Id = id });
+        }
+    }
+}
