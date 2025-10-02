f44c9379b716240c26d34cf9c435a04c51b0136a
Mara Delgadillo <84547390+MaraD98@users.noreply.github.com>
2025-10-02 08:37:20 -0300

    Finalizacion del endpoint GetAll

---
+++ HybridDDDArchitecture/Application/UseCases/Automovil/Queries/GetAllAutomovil/GetAllAutomovilQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.DataTransferObjects;
+using Core.Application;
+
+
+namespace Application.UseCases.Automovil.Queries.GetAllAutomovil
+{
+    public class GetAllAutomovilQuery : QueryRequest<QueryResult<AutomovilDto>>
+    {
+    }
+}
--- HybridDDDArchitecture/Template-API/Controllers/AutomovilController.cs
+++ HybridDDDArchitecture/Template-API/Controllers/AutomovilController.cs
@@ -5,6 +5,7 @@ using Application.UseCases.Automovil.Queries.GetAutomovilById;
 using Application.UseCases.Automovil.Queries.GetAutomovilByChasis;
 using Core.Application;
 using Microsoft.AspNetCore.Mvc;
+using Application.UseCases.Automovil.Queries.GetAllAutomovil;
 
 namespace Controllers
 {
@@ -69,6 +70,14 @@ namespace Controllers
             var automovil = await _commandQueryBus.Send(new GetAutomovilByChasisQuery { NumeroChasis = numeroChasis });
 
             return Ok(automovil);
+        }
+
+        [HttpGet("api/v1/[Controller]")]
+        public async Task<IActionResult> GetAll(uint pageIndex = 1, uint pageSize = 10)
+        {
+            var entities = await _commandQueryBus.Send(new GetAllAutomovilQuery() { PageIndex = pageIndex, PageSize = pageSize });
+
+            return Ok(entities);
         }
     }
 }
